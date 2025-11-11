d3dffac6a64b594b771cb590c6d8e7aeb22dfdab
Luciano Macedo Bergamo <luciano.bergamo@ultrapar.com.br>
2025-11-11 13:33:39 -0300

    inclusão de arquivos html

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/gastos_var')
+def gastos_var():
+    return render_template('gastos_var.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
